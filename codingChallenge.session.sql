6e975eb2c37eae6d4d765f076b1729a4f14cb0d8
Laura <lauracastanovilla@usf.edu>
2025-11-16 19:17:46 -0500

    finishing off task 3

--- codingChallenge.session.sql
+++ codingChallenge.session.sql
@@ -1,18 +1,22 @@
 --TASK 1
--- SELECT c.id, c.first_name, c.last_name, SUM(oi.quantity * oi.unit_price) AS lifetime_spend FROM customers c
--- JOIN order_items oi ON oi.id = c.id
--- GROUP BY c.id, c.first_name, c.last_name
--- ORDER BY lifetime_spend DESC
--- LIMIT 5;
+SELECT c.id, c.first_name, c.last_name, SUM(oi.quantity * oi.unit_price) AS lifetime_spend FROM customers c
+JOIN order_items oi ON oi.id = c.id
+GROUP BY c.id, c.first_name, c.last_name
+ORDER BY lifetime_spend DESC
+LIMIT 5;
 
 --TASK 2
--- SELECT p.category, SUM(p.price * oi.quantity) AS total_revenue FROM products p
--- JOIN order_items oi ON oi.product_id = p.id
--- GROUP BY p.category
--- ORDER BY total_revenue DESC;
+SELECT p.category, SUM(p.price * oi.quantity) AS total_revenue FROM products p
+JOIN order_items oi ON oi.product_id = p.id
+GROUP BY p.category
+ORDER BY total_revenue DESC;
 
--- TASK 3
-SELECT e.id, e.first_name, e.last_name, d.name,  FROM employees e
+--TASK 3
+SELECT e.id, e.first_name, e.last_name, d.name AS department, e.salary FROM employees e
 JOIN departments d ON e.department_id = d.id
-WHERE e.salary > 
-GROUP BY
+JOIN (SELECT department_id, AVG(salary) AS avg_salary FROM employees
+        GROUP BY department_id) dept_avg ON e.department_id = dept_avg.department_id
+WHERE e.salary > dept_avg.avg_salary
+ORDER BY department, e.salary DESC;
+
+--TASK 4
